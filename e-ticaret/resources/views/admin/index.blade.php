40cfc6492a30de622832c092922d8fa605b835f2
onur akyıldız <onurakyildi41@gmail.com>
2023-05-17 01:30:19 +0300

    e-commerce

---
+++ e-ticaret/resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.admin')
+
+@section('title')
+    Admin Paneli
+@endsection
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h1>Robo Raven</h1>
+        </div>
+    </div>
+
+@endsection
